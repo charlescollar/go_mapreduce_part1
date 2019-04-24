236df255f1d8f3615023e3ac9e03d9b39c67f76e
Rebecca Lassiter <rebeccalassiter2020@u.northwestern.edu>
2019-04-24 14:52:10 -0500

    part 2A DONE

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -39,27 +39,29 @@ func schedule(jobName string,
 	//change when you are calling call()
 	//var test = go call(args)
 	//if test = false -> Worker Failed, reassign task
+
+	count:=0
 	var myWaitGroup sync.WaitGroup
 	for i := 0; i < ntasks; i++ {
 		worker:= <-registerChan
+		count++
 		myWaitGroup.Add(1) //increment waitgroup counter
-		
+
 		//task args struct 
 		var myTaskStruct = DoTaskArgs{jobName, mapFiles[i], phase, i, n_other}
-
 		//concurrency on our RPC calls
 		go func(){
 
 			call(worker, "Worker.DoTask", myTaskStruct, nil)
-			registerChan <- worker
 			myWaitGroup.Done() //decrements waitGroup counter
-
+			registerChan <- worker  //workers aren't getting past this on task 18 and 19
+			count--
 		}()
 	}
-	fmt.Printf("out of for")
 	
 	myWaitGroup.Wait()  //stops function from returning until waitgroup counter is at 0 (all tasks are done)
 	fmt.Printf("Schedule: %v done\n", phase)
+	
 }
 
 	
