c22737d786a28be2fd9ac653bd14d162a1d1d150
Rebecca Lassiter <rebeccalassiter2020@u.northwestern.edu>
2019-04-24 14:06:57 -0500

    most of part 2.A

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,6 @@
 package mapreduce
 
-import "fmt"
+import ("fmt"; "sync")
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -11,9 +11,13 @@ import "fmt"
 // suitable for passing to call(). registerChan will yield all
 // existing registered workers (if any) and new ones as they register.
 //
-func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, registerChan chan string) {
+func schedule(jobName string, 
+				mapFiles []string, 
+				nReduce int, 
+				phase jobPhase, 
+				registerChan chan string) {
 	var ntasks int
-	var n_other int // number of inputs (for reduce) or outputs (for map)
+	var n_other int // number of inputs (for reduce) or outputs (for map) 
 	switch phase {
 	case mapPhase:
 		ntasks = len(mapFiles)
@@ -23,7 +27,7 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 		n_other = len(mapFiles)
 	}
 
-
+	//
 	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
 
 	// All ntasks tasks have to be scheduled on workers. Once all tasks
@@ -31,39 +35,32 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// Your code here (Part 2, 2B).
 
-	
-	//Read RegisterChan
-	//create waitGroup
-
-
-	//for each file in mapFiles, assign to worker (registerChanString) by sending RPC
-
-	//send RPC to worker using call() function and DoTaskArgs struct
-	//call(RegChanString, "Worker.DoTask", DoTasksArgs struct, nil)
-
-	//our DoTaskArgs struct if(jobphase = map)
-	//{jobName, mapFiles[i], jobphase, i, n_other}
-	//if(jobphase = reduce) --> leave out the mapFiles[i] argument
-
-	//waitgroup.add(1)  -->increment the waitgroup counter
-
-
-	//when calling call(), need to use concurrency! 
-	//go call(args)
-	//use Scanln() func so program doesn't exit early
-
-	//when worker completes its task, waitgroup.done()  -->decrements waitgroup counter
-	//QUESTION: how to tell when worker completes task? channels? 
-
-	//at the end of func, waitgroup.Wait()  -->waits to return the function until waitgroup counter is at 0 
-	//ie until all tasks are complete
-
-
 	//part B
 	//change when you are calling call()
 	//var test = go call(args)
 	//if test = false -> Worker Failed, reassign task
+	var myWaitGroup sync.WaitGroup
+	for i := 0; i < ntasks; i++ {
+		worker:= <-registerChan
+		myWaitGroup.Add(1) //increment waitgroup counter
+		
+		//task args struct 
+		var myTaskStruct = DoTaskArgs{jobName, mapFiles[i], phase, i, n_other}
 
+		//concurrency on our RPC calls
+		go func(){
 
+			call(worker, "Worker.DoTask", myTaskStruct, nil)
+			registerChan <- worker
+			myWaitGroup.Done() //decrements waitGroup counter
+
+		}()
+	}
+	fmt.Printf("out of for")
+	
+	myWaitGroup.Wait()  //stops function from returning until waitgroup counter is at 0 (all tasks are done)
 	fmt.Printf("Schedule: %v done\n", phase)
 }
+
+	
+
